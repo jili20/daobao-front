11de7034f6bb9123c3841a1f3d5f09178803444a
bing <bing_yu2001@163.com>
2021-03-04 16:11:24 +0800

    帖子列表

--- src/main.js
+++ src/main.js
@@ -12,12 +12,31 @@ import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/app.css'
 import '@/permission'
 
+// 日期格式化第三方库
+import relativeTime from 'dayjs/plugin/relativeTime';
+// 国际化
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs');
+
+// 相对时间插件
+dayjs.extend(relativeTime)
+
+dayjs.locale('zh-cn') // use locale globally
+dayjs().locale('zh-cn').format() // use locale in a specific instance
+
+Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
+
+// Vue.filter('date', (date) => {
+//     return format(new Date(date), 'yyyy-MM-dd')
+// })
+
+
 Vue.use(Buefy);
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
