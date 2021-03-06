f3c0d157a0553fd94dd13dd7728c74e86d9403fb
bing <bing_yu2001@163.com>
2021-03-06 17:42:29 +0800

    编辑 删除 帖子，显示某个标签所有文章

--- src/router/index.js
+++ src/router/index.js
@@ -62,7 +62,14 @@ const routes = [
         name: 'tag',
         path: '/tag/:name',
         component: () => import('@/views/tag/Tag'),
-        meta: { title: '主题列表' }
+        meta: {title: '主题列表'}
+    },
+    // 检索
+    {
+        name: 'search',
+        path: '/search',
+        component: () => import('@/views/Search'),
+        meta: {title: '检索'}
     }
 ]
 
